0409784f8fd894987e8c41f82ad62565d718f64d
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-12-14 14:26:01 +0100

    add onSubmit

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ import { ThemeProvider } from "styled-components";
 import mintTheme from "carbon-react/lib/style/themes/mint";
 import "carbon-react/lib/utils/css";
 import AppWrapper from "carbon-react/lib/components/app-wrapper";
-// import FormikDateRange from "./FormikDateRange";
-// import DialogExample from "./DialogExample";
-// import FormikTest1 from "./formik/FormikTest1";
-import Form2 from "./react-basics/form2";
-import MouseTracker from "./render-props/MouseTracker";
+
+import Intro from "./formik/intro2";
+
 const Index = () => {
   return (
     <ThemeProvider theme={mintTheme}>
       <AppWrapper>
-        <MouseTracker></MouseTracker>
 
-        {/* <FormikDateRange />
-        <p>---</p>
-        <DialogExample />
-        <p>---</p>
-        <FormikTest1 /> */}
+        <Intro />
+        
       </AppWrapper>
     </ThemeProvider>
   );
